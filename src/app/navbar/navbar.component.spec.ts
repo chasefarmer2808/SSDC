99dc131c15dc9e39e418b9baed5173747b4e8f38
Chase Farmer <chasefarmer2808@ufl.edu>
2017-09-09 11:50:01 -0400

    base unit tests

--- src/app/navbar/navbar.component.spec.ts
+++ src/app/navbar/navbar.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavbarComponent } from './navbar.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
@@ -8,7 +11,11 @@ describe('NavbarComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavbarComponent ]
+      declarations: [ NavbarComponent ],
+      imports: [
+        RouterTestingModule,
+        BrowserAnimationsModule 
+      ]
     })
     .compileComponents();
   }));
