c1b8f459e23c1df81476c15345e7376364be0c25
Chase Farmer <chasefarmer2808@ufl.edu>
2017-09-11 23:24:39 -0400

    spec for officers

--- src/app/contact/contact.component.spec.ts
+++ src/app/contact/contact.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ContactComponent } from './contact.component';
+import { OfficersService } from '../services/officers/officers.service';
+import { Officers } from '../services/officers/officers';
+import { Officer } from '../services/officers/officer';
 
 describe('ContactComponent', () => {
   let component: ContactComponent;
@@ -8,7 +11,8 @@ describe('ContactComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ContactComponent ]
+      declarations: [ ContactComponent ],
+      providers: [ OfficersService ]
     })
     .compileComponents();
   }));
@@ -21,5 +25,14 @@ describe('ContactComponent', () => {
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have all officers', () => {
+    expect(component.officers.length).toEqual(Officers.length);
+  });
+
+  it('should show all officers', () => {
+    let officerElements = fixture.nativeElement;
+    expect(officerElements.querySelectorAll('.officer').length).toEqual(component.officers.length);
   });
 });
