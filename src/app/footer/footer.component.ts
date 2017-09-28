b1e5475e1868e451a743d3717cf3f1a259a1e654
Chase Farmer <chasefarmer2808@ufl.edu>
2017-09-28 12:22:03 -0400

    links in footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Routes, Router } from '@angular/router';
+import { routes } from '../app-routing/app-routes';
 
 @Component({
   selector: 'app-footer',
@@ -10,10 +12,16 @@ export class FooterComponent implements OnInit {
   email:string;
   address:string;
   name:string;
+  copywriteYear:number;
+  myRoutes:Routes;
 
   constructor() { }
 
   ngOnInit() {
+    this.email = 'ufssdc@gmail.com';
+    this.name = 'SSDC';
+    this.copywriteYear = new Date().getFullYear();
+    this.myRoutes = routes;
   }
 
 }
