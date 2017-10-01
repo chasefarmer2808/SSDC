f8723595f1cb89f945996ff8ef0605fb191f222c
Chase Farmer <chasefarmer2808@ufl.edu>
2017-09-30 23:31:32 -0400

    footer unit tests

--- src/app/footer/footer.component.spec.ts
+++ src/app/footer/footer.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { FooterComponent } from './footer.component';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { routes } from '../app-routing/app-routes';
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
@@ -8,7 +12,8 @@ describe('FooterComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FooterComponent ]
+      declarations: [ FooterComponent ],
+      imports: [ RouterTestingModule ]
     })
     .compileComponents();
   }));
@@ -21,5 +26,14 @@ describe('FooterComponent', () => {
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have imported routes', () => {
+    expect(component.myRoutes.length).toEqual(routes.length);
+  });
+
+  it('should have populated footer with routes', () => {
+    let navLinks = fixture.nativeElement;
+    expect(navLinks.querySelectorAll('.nav-map-link').length).toEqual(component.myRoutes.length);
   });
 });
