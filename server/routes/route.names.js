23ad2f48b6ff0baaeb90f25003ff891b586eec51
Chase Farmer <chasefarmer2808@ufl.edu>
2018-01-17 22:36:56 -0500

    integration tests for email and listserv complete

--- server/routes/route.names.js
+++ server/routes/route.names.js
@@ -4,5 +4,6 @@ var ROUTE_PREFIX = '/api/v1';
 
 module.exports = {
     emailRoute: `${ROUTE_PREFIX}/email`,
+    listServRoute: `${ROUTE_PREFIX}/email/listserv`,
     facebookRoute: `${ROUTE_PREFIX}/fb`
 };
