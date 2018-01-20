de7b41e47454114621ad542df09181ee3e8b9976
chasefarmer2808 <chasefarmer2808@aol.com>
2018-01-20 00:24:50 -0500

    Develop (#12)
    * frontend refactor
    
    * email using observables
    
    * more unit tests for email form
    
    * dynamic grid with click dropdown:
    
    * plus to cross click animation

--- src/app/officers/officers.component.ts
+++ src/app/officers/officers.component.ts
@@ -20,6 +20,17 @@ import { Officer } from '../services/officers/officer';
         overflow: 'hidden'
       })),
       transition('* => *', animate('100ms ease-in-out'))
+    ]),
+    trigger('plusToCross', [
+      state('0', style({
+        transform: 'none',
+        color: '#37b8eb'
+      })),
+      state('1', style({
+        transform: 'rotate(45deg)',
+        color: '#d9534f'
+      })),
+      transition('* => *', animate('100ms ease-in-out'))
     ])
   ]
 })
@@ -36,5 +47,4 @@ export class OfficersComponent implements OnInit {
   toggleOfficerInfo(officer: Officer) {
     officer.showInfo = !officer.showInfo;
   }
-
 }
