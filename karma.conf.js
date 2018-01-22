c3eca8271cb2f1a3bb3b05c557eec83f51e574f0
Chase Farmer <chasefarmer2808@ufl.edu>
2018-01-22 17:36:18 -0500

    travis fix atempt

--- karma.conf.js
+++ karma.conf.js
@@ -38,7 +38,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Firefox', 'Chrome', 'ChromeCanary'],
+    browsers: ['Firefox'],
     singleRun: false
   };
 
