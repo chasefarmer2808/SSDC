b993c86bef5b07e44b6a408441a8596f6ee1f056
Chase Farmer <chasefarmer2808@ufl.edu>
2018-01-24 18:30:36 -0500

    Material Dialog works on img click

---
+++ src/app/gallery/gallery-grid/image-dialog/image-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image-dialog',
+  templateUrl: './image-dialog.component.html',
+  styleUrls: ['./image-dialog.component.css']
+})
+export class ImageDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
