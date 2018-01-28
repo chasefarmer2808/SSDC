d4f38eb080dada9511fabd78b3c3b6e5083a65a2
Chase Farmer <chasefarmer2808@ufl.edu>
2018-01-27 21:26:13 -0500

    WIP DT page

--- src/app/teams/teams.component.ts
+++ src/app/teams/teams.component.ts
@@ -4,11 +4,21 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-teams',
   templateUrl: './teams.component.html',
-  styleUrls: ['./teams.component.css']
+  styleUrls: ['./teams.component.css', '../app.component.css']
 })
 export class TeamsComponent implements OnInit {
 
   name: string;
+  overview: string;
+  goal: string;
+  teamDescription: string = 
+    `
+    Design teams are a great way to get involved in SSDC!  Members get real hands
+    on experience by participating in competition.  Are available teams are listed
+    below.  Anyone is welcome to join, regardless of skill level.  If any of the teams
+    interest you, express your interest at our next meeting, facebook page, or direct
+    email!
+    `;
 
   constructor(private route: ActivatedRoute) { }
 
