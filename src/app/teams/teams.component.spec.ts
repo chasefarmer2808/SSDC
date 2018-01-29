6a1153c276069f3f51798619343e29de7406955f
Chase Farmer <chasefarmer2808@ufl.edu>
2018-01-28 22:00:40 -0500

    unit tests for teams component

--- src/app/teams/teams.component.spec.ts
+++ src/app/teams/teams.component.spec.ts
@@ -2,13 +2,18 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
 import { HttpModule } from '@angular/http';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Observable } from 'rxjs/Observable';
+import { DebugElement } from '@angular/core';
 
 import { TeamsComponent } from './teams.component';
-import { Observable } from 'rxjs/Observable';
+import { TeamsService } from '../services/teams/teams.service';
+import { TeamsMock } from '../services/teams/teams.mock';
 
 describe('TeamsComponent', () => {
   let component: TeamsComponent;
   let fixture: ComponentFixture<TeamsComponent>;
+  let teamsService: TeamsService;
+  let de: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -23,7 +28,8 @@ describe('TeamsComponent', () => {
           useValue: {
             params: Observable.of({name: 'test'})
           }
-        }
+        },
+        TeamsService
       ]
     })
     .compileComponents();
@@ -32,10 +38,48 @@ describe('TeamsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TeamsComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
+    teamsService = de.injector.get(TeamsService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should return true if empty object', () => {
+    let testObj = {};
+
+    expect(component.isEmpty(testObj)).toBeTruthy();
+  });
+
+  it('should return false of non-empty object', () => {
+    let testObj = {
+      dummp: 'dummy'
+    };
+
+    expect(component.isEmpty(testObj)).toBeFalsy();
+  });
+
+  it('should call getTeams function on component init', async(() => {
+    spyOn(component, 'getTeams');
+    fixture.detectChanges();
+
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      expect(component.getTeams).toHaveBeenCalled();
+    });
+  }));
+
+  it('should get teams from teams service', async(() => {
+    spyOn(teamsService, 'getTeams')
+      .and.returnValue(Observable.of(TeamsMock));
+
+    fixture.detectChanges();
+
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      expect(component.teams.length).toEqual(TeamsMock.length);
+    }); 
+  }))
 });
