70b80a114af92e2b80f9cef56b03466828c5a88d
chasefarmer2808 <chasefarmer2808@ufl.edu>
2018-03-03 17:00:01 -0500

    tests for addUserToListserv

--- src/app/services/email/email.service.spec.ts
+++ src/app/services/email/email.service.spec.ts
@@ -20,6 +20,10 @@ describe('EmailService', () => {
     });
   });
 
+  afterEach(inject([HttpTestingController], (backend: HttpTestingController) => {
+    backend.verify();
+  }));
+
   it('should be created', inject([EmailService], (service: EmailService) => {
     expect(service).toBeTruthy();
   }));
@@ -61,6 +65,45 @@ describe('EmailService', () => {
       });
 
       backend.expectOne(environment.emailUrl).flush(null, { status: 200, statusText: 'ok'});
+  })));
+
+  it('should send an expected listserv request', async(inject([EmailService, HttpTestingController],
+    (service: EmailService, backend: HttpTestingController) => {
+      let mockEmail = new Email('test@test.com', 'John', 'Smith', 'Hello');
+
+      service.addUserToListserv(mockEmail).subscribe();
+
+      backend.expectOne((req: HttpRequest<any>) => {
+        const body = req.body;
+        return req.url === environment.listservUrl
+          && req.method === 'POST'
+          && body.emailAddress === 'test@test.com'
+          && body.firstName === 'John'
+          && body.lastName === 'Smith'
+          && body.body === 'Hello';
+      }, `POST to ${environment.emailUrl} with mock email form information`);
+  })));
+
+  it('should emit false for 500 Server Error', async(inject([EmailService, HttpTestingController],
+    (service: EmailService, backend: HttpTestingController) => {
+      let mockEmail = new Email('test@test.com', 'John', 'Smith', 'Hello');
+
+      service.addUserToListserv(mockEmail).subscribe((next) => {
+        expect(next).toBeFalsy();
+      });
+
+      backend.expectOne(environment.listservUrl).flush(null, { status: 500, statusText: 'Server Error'});
+  })));
+
+  it('should emit true for 200 OK', async(inject([EmailService, HttpTestingController],
+    (service: EmailService, backend: HttpTestingController) => {
+      let mockEmail = new Email('test@test.com', 'John', 'Smith', 'Hello');
+
+      service.addUserToListserv(mockEmail).subscribe((next) => {
+        expect(next).toBeTruthy();
+      });
+
+      backend.expectOne(environment.listservUrl).flush(null, { status: 200, statusText: 'ok'});
   })));
 
 
