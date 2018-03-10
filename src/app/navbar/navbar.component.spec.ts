962438b3f0e0f2b2a0304335d05be9fcde06b720
chasefarmer2808 <chasefarmer2808@ufl.edu>
2018-03-10 13:03:47 -0500

    fix scrolling and add tests

--- src/app/navbar/navbar.component.spec.ts
+++ src/app/navbar/navbar.component.spec.ts
@@ -8,6 +8,8 @@ import { Ng2PageScrollModule } from 'ng2-page-scroll';
 import { HttpClientModule } from '@angular/common/http';
 import { RouteFilterPipe } from '../pipes/route-filter/route-filter.pipe';
 
+import { routes } from '../app-routing/app-routes';
+
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
   let fixture: ComponentFixture<NavbarComponent>;
@@ -28,7 +30,57 @@ describe('NavbarComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
+    // fixture.detectChanges();
+  });
+
+  it('should call populateTeamsSubItems on init', () => {
+    let populateTeamsSubItemsSpy = spyOn(component, 'populateTeamsSubItems').and.callThrough();
+
     fixture.detectChanges();
+
+    fixture.whenStable().then(() => {
+      expect(populateTeamsSubItemsSpy).toHaveBeenCalled();
+    });
+  });
+
+  it('should get contact route by path name', () => {
+    fixture.detectChanges();
+    let trueRoute = routes[1];
+
+    let testRoute = component.getRoute('contact');
+
+    expect(testRoute).toEqual(trueRoute);
+  });
+
+  it('should return false for non existant route', () => {
+    fixture.detectChanges();
+    let testRoute = component.getRoute('does not exist');
+    expect(testRoute).toBeFalsy();
+  });
+
+  it('should toggle navDropDown boolean', () => {
+    fixture.detectChanges();
+    expect(component.navDropDown).toBeFalsy();
+    component.toggleNav();
+    expect(component.navDropDown).toBeTruthy();
+  });
+
+  it('should call collapseAll on closeNav call if collapse is true', () => {
+    fixture.detectChanges();
+    let collapseAllSpy = spyOn(component, 'collapseAll').and.callThrough();
+
+    component.closeNav(true, 'home');
+
+    expect(collapseAllSpy).toHaveBeenCalled();
+  });
+
+  it('should not call collapseAll on closeNav call if collapse is false', () => {
+    fixture.detectChanges();
+    let collapseAllSpy = spyOn(component, 'collapseAll').and.callThrough();
+
+    component.closeNav(false, 'home');
+
+    expect(collapseAllSpy).toHaveBeenCalledTimes(0);
   });
 
 });
