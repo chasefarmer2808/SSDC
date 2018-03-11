29085972f6234e31261ef9f7a730b8968eb8b6cc
chasefarmer2808 <chasefarmer2808@ufl.edu>
2018-03-11 13:43:19 -0400

    prevents duplicate users

--- server/schemas/user.js
+++ server/schemas/user.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose');
 var userSchema = mongoose.Schema({
     username: {
         type: String,
+        unique: true,
         required: true
     },
     password: {
         type: String,
+        unique: true,
         required: true
     },
     isAdmin: {
